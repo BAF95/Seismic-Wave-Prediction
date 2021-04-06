5dae39f2c2ba2d8ea03a8aaa4cc110ac70d720f5
BAF95 <75345683+BAF95@users.noreply.github.com>
2021-04-05 17:55:29 -0700

    Updated

--- seismic data.py
+++ seismic data.py
@@ -1,33 +1,36 @@
 import requests
-import json
-import os
-from pprint import pprint
-import sklearn as sk
+
+
+
 import pandas as pd
-from sklearn import naive_bayes
+
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
-from sklearn.metrics import accuracy_score
 
-from sklearn.naive_bayes import BaseNB
 from sklearn.model_selection import train_test_split
-url = "https://earthquake.usgs.gov/fdsnws/event/1/[query[format=geojson]]"
-data_url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-01-31&eventtype=earthquake"
-output_path = os.path.join("/images/")
 
+# url: references default USGS API url
+# data_url = the actual data url
+
+url = "https://earthquake.usgs.gov/fdsnws/event/1/[query[format=geojson]]"
+data_url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-4-01&endtime=2020-5-01&eventtype=earthquake"
+
+# json response and data format
 response = requests.get(data_url)
 data = response.json()
 
-#needed_data = ["magType", "mag", "dmin"]
+# needed_data = ["magType", "mag", "dmin"]
 # mag = data["features"][0]["properties"]["mag"]
 # magType = data["features"][0]["properties"]["magType"]
 # dmin = data["features"][0]["properties"]["dmin"]
-# pprint(data)
+
+# creates lists for needed data
 dmin = []
 mag = []
 magType = []
 none = []
 
+#wrapper to extract data from GEOJson
 for x in data["features"]:
     if x["properties"]["dmin"] == None:
         none.append((x["properties"]["dmin"]))
@@ -44,15 +47,14 @@ for x in data["features"]:
     else:
         magType.append(x["properties"]["magType"])
 
-# creates dataframe from list
+# creates dataframe from lists
 dmin_Df = pd.DataFrame({"Depth min": dmin})
-
-
 mag_df = pd.DataFrame({"Magnitude":mag})
 mag_df["Magnitude"] = mag_df["Magnitude"].abs()
-
-
 magType_df = pd.DataFrame({"Waveform":magType})
+
+# used to show statistics distribution of prediction variables
+# a good distribution of wave forms is important
 print(magType_df.value_counts())
 
 # merges fataframes made above
@@ -65,23 +67,29 @@ Ses_DF = Ses_DF.merge(magType_df, "inner", right_index=True, left_index=True)
 
 
 
-# print(Ses_DF)
+# sets variables to be predicited
 X = Ses_DF[["Magnitude", "Depth min"]]
-# X = Ses_DF["Magnitude"]
-
 y = Ses_DF["Waveform"]
+
+# test train split
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+# model creation
 clf = MultinomialNB()
 
+# fits data
 clf.fit(X_train, Y_train)
-# MultinomialNB()
+
+# scores model
 score = clf.score(X_test, Y_test)
 print(f"Accuracy of model: {score*100}%")
+# predicted model
 predicted = clf.predict(X)
 
-# score = predicted.score(X,y)
+
 print(predicted)
 
+# EXTRA CODE FOR FUTURE USE
+# score = predicted.score(X,y)
 # score = clf.score(X_test,Y_test)
 # print(score)
 # NB = naive_bayes.GaussianNB()
