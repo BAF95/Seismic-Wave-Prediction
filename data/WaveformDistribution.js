5165a7acd54cd7b9033b81bfe20a75b7a07b3c1b
BAF95 <75345683+BAF95@users.noreply.github.com>
2021-04-05 21:58:07 -0700

    Updated9:58PM4/5/2021

---
+++ data/WaveformDistribution.js
@@ -0,0 +1,103 @@
+function makeResponsive() {
+
+// Define SVG area dimensions
+  var svgWidth = 960;
+  var svgHeight = 660;
+
+// Define the chart's margins as an object
+  var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 40
+  };
+
+// Define dimensions of the chart area
+  var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+  var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+  var svg = d3.select("body")
+      .append("svg")
+      .attr("height", svgHeight)
+      .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+  var chartGroup = svg.append("g")
+      .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from PredictionData.csv
+  d3.csv("Waveformtotals.csv").then(function (sesData) {
+
+    console.log(sesData);
+
+    // Cast the hours value to a number for each piece of tvData
+    sesData.forEach(function (d) {
+      d.Count = +d.Count
+    });
+
+    // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+    var xBandScale = d3.scaleBand()
+        .domain(sesData.map(d => d.Waveform))
+        .range([0, chartWidth])
+        .padding(0.1);
+
+    // Create a linear scale for the vertical axis.
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(sesData, d => d.Count)])
+        .range([chartHeight, 0]);
+
+    // Create two new functions passing our scales in as arguments
+    // These will be used to create the chart's axes
+    var bottomAxis = d3.axisBottom(xBandScale);
+    var leftAxis = d3.axisLeft(yLinearScale).ticks(8);
+
+    // Append two SVG group elements to the chartGroup area,
+    // and create the bottom and left axes inside of them
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+
+    // Create one SVG rectangle per piece of PredictionData
+    // Use the linear and band scales to position each rectangle within the chart
+    var Rectgroup = chartGroup.selectAll(".bar")
+        .data(sesData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => xBandScale(d.Waveform))
+        .attr("y", d => yLinearScale(d.Count))
+        .attr("width", xBandScale.bandwidth())
+        .attr("height", d => chartHeight - yLinearScale(d.Count));
+
+
+    // Step 1: Append tooltip div
+    var toolTip = d3.select("body")
+        .append("div")
+        .classed("tooltip", true);
+
+    // Step 2: Create "mouseover" event listener to display tooltip
+    Rectgroup.on("mouseover", function (d) {
+      toolTip.style("display", "block")
+          .html(
+              `<strong>Total Waveforms<strong><hr>${d.Count}`)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px");
+    })
+        // Step 3: Create "mouseout" event listener to hide tooltip
+        .on("mouseout", function () {
+          toolTip.style("display", "none");
+        });
+
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive)
--- seismic data.py
+++ seismic data.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-
+import matplotlib as plt
 # url: references default USGS API url
 # data_url = the actual data url
 
@@ -43,13 +43,17 @@ for x in data["features"]:
         magType.append(x["properties"]["magType"])
 
 # creates dataframe from lists
-dmin_Df = pd.DataFrame({"Depth min": dmin})
+dmin_Df = pd.DataFrame({"Depth": dmin})
 mag_df = pd.DataFrame({"Magnitude":mag})
 mag_df["Magnitude"] = mag_df["Magnitude"].abs()
 magType_df = pd.DataFrame({"Waveform":magType})
 
+print(dmin_Df.max(), dmin_Df.min())
 # used to show statistics distribution of prediction variables
 # a good distribution of wave forms is important
+Waveformtotals = magType_df.value_counts()
+Waveformtotals = Waveformtotals.rename("Count")
+Waveformtotals.to_csv(path_or_buf="data/Waveformtotals.csv")
 print(magType_df.value_counts())
 
 # merges fataframes made above
@@ -63,7 +67,7 @@ Ses_DF = Ses_DF.merge(magType_df, "inner", right_index=True, left_index=True)
 
 
 # sets variables to be predicited
-X = Ses_DF[["Magnitude", "Depth min"]]
+X = Ses_DF[["Magnitude", "Depth"]]
 y = Ses_DF["Waveform"]
 
 # test train split
@@ -79,13 +83,53 @@ score = clf.score(X_test, Y_test)
 print(f"Accuracy of model: {score*100}%")
 # predicted model
 predicted = clf.predict(X)
+predicted = pd.DataFrame({"Predicted_Waveform":predicted})
 
 
-print(predicted)
+compared = predicted.merge(y, "inner", right_index=True, left_index=True)
+# compared = compared.merge(matchDF, "inner", right_index=True, left_index=True)
+print(compared)
 
+compared_accuracy = []
+i = 0
+i_list = []
+while i < len(compared):
+    value = compared.loc[i]
+    i += 1
+    value = value.T
+    com1 = value["Waveform"]
+    com2 = value["Predicted_Waveform"]
+    if com1 == com2:
+       compared_accuracy.append("True")
+       i_list.append(i)
+
+
+    else:
+       compared_accuracy.append("False")
+       i_list.append(i)
+compared_accuracyDF = pd.DataFrame({"Match": compared_accuracy})
+i_listDF = pd.DataFrame({"Index": i_list})
+compared = compared.merge(i_listDF, "inner", right_index=True, left_index=True)
+compared = compared.merge(compared_accuracyDF, "inner", right_index=True, left_index=True)
+compared = compared.merge(X, "inner", right_index=True, left_index=True)
+
+
+print(compared)
+
+
+compared.to_csv(path_or_buf="data/PredictionData.csv")
+
+Accurate_Predicted_Total = compared["Match"].value_counts()
+Accurate_Predicted_Total = Accurate_Predicted_Total.T
+Accurate_Predicted_Total["Actual Percentage"] = (Accurate_Predicted_Total["True"]/compared["Index"].max())*100
+Accurate_Predicted_Total["Theoretical Percentage"] = score*100
+Accurate_Predicted_Total["Percent Error"] = (((score*100)-(Accurate_Predicted_Total["True"]/compared["Index"].max())*100)/(score*100))*100
+Accurate_Predicted_Total.to_csv(path_or_buf="data/Keystats.csv")
+print(Accurate_Predicted_Total)
 # EXTRA CODE FOR FUTURE USE
 # score = predicted.score(X,y)
 # score = clf.score(X_test,Y_test)
 # print(score)
 # NB = naive_bayes.GaussianNB()
 
+# plot = plt.plot()
